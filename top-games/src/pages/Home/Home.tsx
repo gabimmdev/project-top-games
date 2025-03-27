ef34e2d709ac3f40fe5c11da1d6a27204e8882bb
gabimezze <gabrielagomescezar@gmail.com>
2025-03-27 09:51:37 -0300

    feat: import NavBar

--- top-games/src/pages/Home/Home.tsx
+++ top-games/src/pages/Home/Home.tsx
@@ -1,8 +1,10 @@
+import NavBar from '@/components/NavBar/NavBar'
 import Hero from '@/pages/Home/sections/Hero/Hero'
 
 const Home = () => {
     return (
       <>
+        <NavBar/>
         <Hero />
       </>
     )
